c683114028556969600564a51fdd9d3f3f85e043
MACHIREDDYVISHNUVARD <MACHIREDDYVISHNUVARD@IBM-GH0WR24>
2026-04-01 16:35:35 +0530

    Add project files.

---
+++ Legend_clinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Legend_clinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
