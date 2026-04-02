8dadc2de4ce7a0080a7720df86ecc56c29d8f534
TharunTBasineni <tharunbasineni660@gmail.com>
2026-04-02 14:55:21 +0530

    added a controller

---
+++ Legend_clinic/Controllers/HomeController3.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Legend_clinic.Controllers
+{
+    public class HomeController3 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
